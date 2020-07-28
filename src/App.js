1e189dee6b31d74435c4935229193e61cbd349b4
NithinLukose <nithinclukose12@gmail.com>
2020-07-28 20:14:08 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -51,34 +51,68 @@ function App() {
             <span>Skills</span>
           </div>
 
-          <div className="tech">
-            <p>HTML</p>
-          </div>
-          
-          <div class="container">
-            <div class="skill avg">90%</div>
-          </div>
+          <div className='tech'>
 
-          <div className="tech">
-            <p>CSS</p>
-          </div>
-          
-          <div class="container">
-            <div class="skill avg">90%</div>
-          </div>
+           <div>HTML</div>
+           <div>CSS</div>
+           <div>Javascript</div>
+           <div>React js</div>
+           <div>Redux</div>
+           <div>Data Structure</div>
+           <div>Web Development</div>
+           <div>Frontend Development</div>
 
-          <div className="tech">
-            <p>Javascript</p>
-          </div>
 
-          <div class="container">
-            <div class="skill expert">95%</div>
           </div>
 
         </div>
 
       </div>
-      <div className="right-div">professional details</div>
+      <div className="right-div">
+
+        <div className="exp">
+          <div className="details">
+            <i className="material-icons">work</i> 
+            <span>Work Experience</span>
+          </div>
+          <div className="company">
+
+            <h3>Front End Developer / Infosys</h3>
+            <p>May 2018 - <span className="current">Current</span></p>
+            <p>Software Engineer with 2+ year experience in developing and maintaining web applications. Adept at overseeing all phases of software development 
+              life-cycle, from requirements gathering, design and implementation
+               to maintenance.
+            </p>
+
+          </div>
+        </div>
+        <div className="exp">
+
+          <div className="details">
+            <i className="material-icons">school</i> 
+            <span>Education</span>
+          </div>
+
+          <div className="company">
+
+            <h3>Silicon Institute of Technology</h3>
+            <p>Aug 2014 - Apr 2018</p>
+            <p>BTECH - Computer Science</p>
+            <p>8.19 CGPA</p>
+
+          </div>
+
+          <div className="company">
+
+            <h3>Kendriya Vidyalaya NO. 4</h3>
+            <p>Aug 2013 - Apr 2014</p>
+            <p>Computer Science</p>
+            <p>84%</p>
+
+          </div>
+
+        </div>
+      </div>
 
 
     </div>

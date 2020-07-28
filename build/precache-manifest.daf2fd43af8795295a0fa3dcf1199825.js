9d0d6bc2841a40ec64bc2d7d43887ed8c592aa7f
NithinLukose <nithinclukose12@gmail.com>
2020-07-28 20:29:33 +0530

    build directory

---
+++ build/precache-manifest.daf2fd43af8795295a0fa3dcf1199825.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c200edde26d78005756dab7db4c306",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7aecdac1a47da349eaf3",
+    "url": "/static/css/main.83077cda.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aecdac1a47da349eaf3",
+    "url": "/static/js/main.182c93cf.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "1746a4fef0419a1d493c402a38842603",
+    "url": "/static/media/display_image.1746a4fe.jpg"
+  }
+]);
